e4776d348af4833e3bd1145fd93c48b100474e63
e13u <54067494+e13u@users.noreply.github.com>
2019-09-09 17:49:19 -0300

    09/09/19
    -Menu e tela de seleção de tema
    -Lista de temas completa
    -Faltando enviar para o main game

---
+++ Assets/Scripts/ThemeSelection/ThemeButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ThemeButton : MonoBehaviour
+{
+    public Sprite notSelectedTheme;
+    public Sprite selectedTheme;
+    public int themeId = 0;
+    public bool onOff = false;
+
+    private Image buttonImage;
+    void Start(){
+        buttonImage = GetComponent<Image>();
+    }
+    public void TurnThemeOnOff(){
+        onOff = !onOff;
+        //onOff == true ? buttonImage.sprite = selectedTheme: buttonImage.sprite = notSelectedTheme;
+        if(onOff){
+            buttonImage.sprite = selectedTheme;
+        }
+        else{
+            buttonImage.sprite = notSelectedTheme;
+        }
+            ThemeManager.Instance.AddRemoveThemeList(themeId);
+    }
+}
---
+++ Assets/Scripts/ThemeSelection/ThemeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeManager : MonoBehaviour
+{
+    public int selectedThemesQuantity = 0;
+    public List<int> themeList = new List<int>();
+    public static ThemeManager Instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddRemoveThemeList(int themeId){
+        if(!themeList.Contains(themeId)){
+            themeList.Add(themeId);
+        }
+        else{
+            themeList.Remove(themeId);
+        }
+        themeList.Sort();
+    }
+}
